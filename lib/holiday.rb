0c06adf49df7fb29f9903a207af7d722b0e8b6a9
Kevin K <doilerjar@outlook.com>
2018-08-08 05:16:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,7 +96,6 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{k.to_s.capitalize}:"
       v.each do |key,value|
         print "  #{key.to_s.capitalize}: "
-        binding.pry 
         result << value
       end 
     end
